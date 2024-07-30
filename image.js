d890215634d2515d7daa5743cf7801f103233a3f
Furkan Bitik <nusretbeykonagi@gmail.com>
2024-07-30 19:26:33 +0300

    Update

--- image.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const hamburgerMenu = document.getElementById("hamburger-menu");
-  const navLinks = document.getElementById("nav-links");
-
-  hamburgerMenu.addEventListener("click", function() {
-    navLinks.classList.toggle("active");
-  });
-});
