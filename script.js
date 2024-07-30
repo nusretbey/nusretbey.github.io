0caca245d5b10b9fae1bf1ca2ee609a992136eab
Furkan Bitik <nusretbeykonagi@gmail.com>
2024-07-30 22:56:49 +0300

    Update

--- script.js
+++ script.js
@@ -6,19 +6,3 @@ document.addEventListener("DOMContentLoaded", function() {
 	  navLinks.classList.toggle('active');
 	});
   });
-
-  document.addEventListener('DOMContentLoaded', function() {
-	const carousels = document.querySelectorAll('.carousel');
-	carousels.forEach(function(carousel) {
-	  const prevButton = carousel.querySelector('.carousel-control-prev');
-	  const nextButton = carousel.querySelector('.carousel-control-next');
-	  
-	  prevButton.addEventListener('click', function() {
-		$(carousel).carousel('prev');
-	  });
-  
-	  nextButton.addEventListener('click', function() {
-		$(carousel).carousel('next');
-	  });
-	});
-  });
